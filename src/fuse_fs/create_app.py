02559c62d10501aa7072a1c9135aa31d20917340
Alena Roshkovskya <alenasere4enko@yandex.ru>
2021-11-09 01:57:59 +0300

    add line update

--- src/fuse_fs/create_app.py
+++ src/fuse_fs/create_app.py
@@ -1,3 +1,8 @@
+from threading import Thread
+from random import uniform
+from time import sleep
+
+from .consts import DEVIATION_FROM_POINT, BASE_LINE_VOLTAGE_POS, LINE_VOLTAGE_DELTA
 from .filesystem import Filesystem
 
 DELIMETR = "/"
@@ -5,15 +10,27 @@ DELIMETR = "/"
 def create_app():
     fs = mount_fs()
 
+    terminal_proc = Thread(target=run_fs, args=(fs,))
+    voltage_updater_proc = Thread(target=update_line_voltage, args=(fs,))
+    
+    terminal_proc.start()
+    voltage_updater_proc.start()
+    
+    terminal_proc.join()
+    voltage_updater_proc.join()
+    
+
+
+def run_fs(filesystem: Filesystem):
     while True:
         try:
-            command = input(f"{fs.name}> ")
-            terminal(fs, command)
+            command = input(f"{filesystem.name}> ")
+            terminal(filesystem, command)
         except KeyboardInterrupt:
             print("End of session")
         except Exception as exc:
             print(exc)
-
+    
 
 def mount_fs() -> Filesystem:
     return Filesystem()
@@ -40,4 +57,16 @@ def terminal(filesystem: Filesystem, command: str) -> None:
         filesystem.remove_dir(dir_name)
     elif "cat" in splitted_command:
         filesystem.cat_file(dir_name)
-    
+
+
+def update_line_voltage(filesystem: Filesystem):
+    while True:
+        for dir in filesystem.dirs:
+            for file in dir.files:
+                if file.name.startswith("line"):
+                    file.content = uniform(
+                        BASE_LINE_VOLTAGE_POS - LINE_VOLTAGE_DELTA, 
+                        BASE_LINE_VOLTAGE_POS + LINE_VOLTAGE_DELTA
+                    ) * DEVIATION_FROM_POINT
+                    
+        sleep(1)
--- src/fuse_fs/filesystem.py
+++ src/fuse_fs/filesystem.py
@@ -40,4 +40,8 @@ class Filesystem:
             target_dir.files
         ), None)
 
+        if not target_file:
+            print("No such file")
+            return
+
         print(target_file.content)
